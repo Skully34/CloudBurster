244c882ee41c58e4e75e518a63a3f6d53feec4c1
Jeff Lin <jeff.lin.97@gmail.com>
2025-01-12 02:06:07 -0800

    add barebones gun selector

---
+++ Assets/Scripts/PlayerGunSelector.cs
@@ -0,0 +1,21 @@
+using Guns.Gun;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PlayerGunSelector : MonoBehaviour
+{
+    [SerializeField] GunScriptableObject Gun;
+
+    void Start()
+    {
+        Gun.Spawn(this.transform, this);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Gun.Shoot();
+        }
+    }
+}
