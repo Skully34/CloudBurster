cd3a7a138fa69796c51835359639e9d262f2dd21
Marcus W <rks@whq.ca>
2025-01-15 17:06:31 -0800

    Base Health & Hit Effect Framework
    Added a script with methods for Health, damage and freeze. Not complete

--- Assets/Scripts/Health/BaseHealthHit.cs
+++ Assets/Scripts/Health/BaseHealthHit.cs
@@ -2,15 +2,105 @@ using UnityEngine;
 
 public class BaseHealthHit : MonoBehaviour
 {
-    //// Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-        
-    //}
 
-    //// Update is called once per frame
-    //void Update()
-    //{
+    [SerializeField] float health;
+
+    [SerializeField] float immunityTime;
+    float immunityTimeActual;
+    bool immune;
+
+    int freezeAmount;
+    [SerializeField] int freezeLimit;
+    [SerializeField] float freezeTime;
+    float freezeTimeActual;
+    bool frozen;
+    [SerializeField] bool MC;
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        //damage immunity timer
+        if (immune)
+        {
+            immunityTimeActual -= Time.deltaTime;
+        }
+        if (immunityTimeActual < 0)
+        {
+            immune = false;
+        }
+
+        //freeze logic
+        if (freezeAmount > freezeLimit)
+        {
+            freezeTimeActual = freezeTime;
+            frozen = true;
+            rb.simulated = false;
+            FreezeEvent();
+        }
+
+     if (frozen)
+        {
+            freezeTimeActual -= Time.deltaTime;
+        }
+     if (frozen && freezeTimeActual <= 1)
+        {
+            frozen = false;
+            rb.simulated = true;
+            UnFreezeEvent();
+        }
         
-    //}
+    }
+
+    // damage is float to account for super low damage attacks, but wanting to keep health in simple numbers for the MC.
+    public void Hit(float damage, int freezeAtk)
+    {
+        if (!immune) {
+            if (MC)
+            {
+                if (damage >= 1)
+                {
+                    damage = 1;
+                }
+                health -= damage;
+                immune = true;
+                immunityTimeActual = immunityTime;
+            }
+            else if (!MC)
+            {
+                health -= damage;
+            }
+
+            freezeAmount += freezeAtk;
+            
+            if (health <= 0)
+            {
+                DeathEvent();
+            }
+        }
+    }
+    public void FreezeEvent()
+    {
+        //method for freeze triggers
+    }
+    public void UnFreezeEvent()
+    {
+        //method for unfreeze triggers
+    }
+    public void DeathEvent()
+    {
+        if (MC)
+        {
+
+        }
+        if (!MC)
+        {
+            Destroy(this);
+        }
+    }
 }
